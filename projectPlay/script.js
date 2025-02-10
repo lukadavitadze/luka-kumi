e05d33bae4183ca20a14c3557cbc5051e7a9c6f0
luka davitadze <lukinio2222@gmail.com>
2025-02-10 18:00:21 +0400

    add top plays

---
+++ projectPlay/script.js
@@ -0,0 +1,20 @@
+// Array of images and corresponding text descriptions
+const images = [
+    { src: 'images/pic03.jpg', text: 'Image 6 Description' },
+    { src: 'images/pic02.jpg', text: 'Image 2 Description' },
+    { src: 'images/pic04.jpg', text: 'Image 3 Description' },
+    { src: 'images/pic05.jpg', text: 'Image 4 Description' },
+    { src: 'images/pic06.jpg', text: 'Image 5 Description' }
+];
+
+// Current image index
+let currentIndex = 0;
+
+function changeImage() {
+    // Increment the index
+    currentIndex = (currentIndex + 1) % images.length;
+
+    // Change the image source and text description
+    document.getElementById('image').src = images[currentIndex].src;
+    document.getElementById('text-overlay').innerText = images[currentIndex].text;
+}
